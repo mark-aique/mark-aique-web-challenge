2907951bfc4de2a515ec2013bd3cc83b972c8d95
mark-aique <mark.cabalona@aiqueinnovation.com>
2025-02-13 13:13:45 +0800

    feature: Implement IntroductionSection component
    - add typography styles and assets for IntroductionSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 'use client'
+import style from "@/styles/styles.module.css";
+import IntroductionSection from "./components/introduction_section";
 import Navbar from "./components/nav_bar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <div className={style.content}>
+        <div className={"mx-[12px] my-[12px] md:my-[24px] max-w-[1480px]"}>
+          <IntroductionSection />
+        </div>
+      </div>
     </>
   );
 }
